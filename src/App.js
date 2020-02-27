0d6d1d17003da032c791eb8757b378bffb4cc542
Adrian Guadalupe <safisweetkeyz@gmail.com>
2020-02-27 15:56:48 -0500

    made login form, added reactstrap and availity-reactstrap-validation dependencies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import Login from './components/Login'
 
 function App() {
   return (
     <div className="App">
       <h1>Golden Tutor</h1>
+      <Login />
     </div>
   );
 }
