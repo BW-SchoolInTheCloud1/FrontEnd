feda4b1300bf3fb283ca2e7822336450af5b2446
karapeoples <52808542+karapeoples@users.noreply.github.com>
2020-02-29 21:46:47 -0600

    Volunteer Register not quite and Log Out This commit: ~~Code is in for Register but not quite functional ~~Upgrade to all Dependencies to see about errors ~~I think it needs a few items to not errors but get rid of warnings ~~Added a Few Extra Things
    Because:
    ~~Fixing to see about help to finish the register functionality

--- src/App.js
+++ src/App.js
@@ -1,35 +1,31 @@
-import React from 'react'
-import "./App.css"
-import { Route } from 'react-router-dom'
-import ProtectedRoute from './utils/ProtectedRoute'
-import NavBar from './components/NavBar'
-import Login from './components/Login'
-import StudentSignUp from './components/StudentSignUp'
-import SeniorSignUp from './components/SeniorSignUp'
-import AdminSignUp from './components/AdminSignUp'
-import Register from './components/Register'
-import StudentDash from './components/StudentDash'
-import AdminDash from './components/AdminDash'
-import DashBoards from './components/DashBoards'
-import SeniorDash from './components/SeniorDash'
+import React from 'react';
+import './App.css';
+import { Route } from 'react-router-dom';
+import ProtectedRoute from './utils/ProtectedRoute';
+import NavBar from './components/NavBar';
+import Login from './components/Login';
+import StudentSignUp from './components/StudentSignUp';
+import SeniorSignUp from './components/SeniorSignUp';
+import AdminSignUp from './components/AdminSignUp';
+import Register from './components/Register';
+import StudentDash from './components/StudentDash';
+import AdminDash from './components/AdminDash';
+import DashBoards from './components/DashBoards';
+import SeniorDash from './components/SeniorDash';
 
 function App() {
 	return (
 		<div className='App'>
-         <NavBar />
-         <ProtectedRoute exact path ='/student-dash' component={StudentDash}/>
-			<ProtectedRoute exact path ='/admin-dash' component={AdminDash}/>
+			<NavBar />
+			<ProtectedRoute exact path='/student-dash' component={StudentDash} />
+			<ProtectedRoute exact path='/volunteer-dash' component={SeniorDash} />
+			<ProtectedRoute exact path='/admin-dash' component={AdminDash} />
 			<Route exact path='/' component={Login} />
 			<Route path='/register' component={Register} />
 			<Route path='/student' component={StudentSignUp} />
 			<Route path='/volunteer' component={SeniorSignUp} />
-
-            
-            <Route path='/volunteer-dash' component={SeniorDash}/>
-
-         <Route path='/admin-signup' component={AdminSignUp} />
-         <Route path='/dashboards' component={DashBoards}/>
-
+			<Route path='/admin-signup' component={AdminSignUp} />
+			<Route path='/dashboards' component={DashBoards} />
 		</div>
 	);
 }
--- src/components/StudentDash.js
+++ src/components/StudentDash.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Button } from 'reactstrap';
-import {useHistory} from 'react-router-dom'
+
+
 
 const BackgroundDiv = styled.div`
 	height: 100vh;
@@ -12,23 +12,16 @@ const BackgroundDiv = styled.div`
 	background-image: linear-gradient(180deg, #fcb97d 25%, #e07a5f 100%);
 `;
 
-// const handleClick = () => {
-//   return (
-//     localStorage.removeItem('token'),
-//     history.push('/'),
-//   )
-// }
+
 
 const StudentDash = () => {
-  const history = { useHistory }
-  
+	
+	
+
   return (
 			<BackgroundDiv>
 				<div>
 					<input type='search' value='' />
-				</div>
-				<div>
-					<Button>Log Out</Button>
 				</div>
 			</BackgroundDiv>
 		);
