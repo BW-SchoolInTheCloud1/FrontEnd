bdb7d2cdf0dc009c4e81b0dadedb72487b923185
karapeoples <52808542+karapeoples@users.noreply.github.com>
2020-03-03 11:26:26 -0600

    :rainbow: Student Search and Senior Card Styled :rainbow: This commit: ~~Semantic-UI-React Installed ~~Student Search Bar on Student Dash Styled ~~Senior Card Styled with Placeholder Image ~~Placeholder Image may be replaced as talked about
    Because:
    --{Moving on to Admin Dash Styles for Toggle and to Push this Styled Code}

--- src/components/senior/SeniorCard.js
+++ src/components/senior/SeniorCard.js
@@ -1,17 +1,32 @@
 import React from 'react';
-
+import img from '../../images/ph.bmp';
+import { Card, Image } from 'semantic-ui-react';
 
 
 const SeniorCard = ({ firstName, lastName, times, location, user_id }) => {
   
-  return (
-    <div key={user_id}>
-      <h1>{firstName}{lastName}</h1>
-      <h3>Available: {times}</h3>
-      <h3>Location: {location}</h3>
-      <h4>Id: {user_id}</h4>
-      </div>
-  )
+	return (
+		<div className='col'>
+			<Card key={user_id} className='cardWrapper'>
+			<Card.Content header >
+				<span className='imgDiv'>
+					<div>
+				<Card.Content>{firstName}</Card.Content>
+				<Card.Content>{lastName}</Card.Content>
+					</div>
+					<div>
+						<Image src={img} alt='avatar' avatar className='img' />
+					</div>
+				</span>
+				</Card.Content>
+				<Card.Content>
+					<h3>Available: {times}</h3>
+					<h3>Location: {location}</h3>
+				</Card.Content>
+				<Card.Content extra>Id: {user_id}</Card.Content>
+			</Card>
+		</div>
+		);
 }
 
 export default SeniorCard
