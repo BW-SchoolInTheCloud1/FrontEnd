5e2eaa7db7461049430d323d9d5e36b3a2c221fa
karapeoples <52808542+karapeoples@users.noreply.github.com>
2020-03-04 15:01:40 -0600

    Search Bars Started This commit: ~~Search Bar Starting Functionality Included ~~Styling for Search Included ~~Have to chase down a :bug: in the search function
    Because:
    --{It is getting close to time to pair code again and I'm happy with what I've got put in gives us plenty of work to look at}

--- src/components/admin/TaskList.js
+++ src/components/admin/TaskList.js
@@ -6,18 +6,34 @@ import TaskCard from './TaskCard'
 import AdminDash from './AdminDash'
 
 const TaskList = () => {
-  	const tasks = useSelector(state => state.tasks);
-	  
+  const tasks = useSelector(state => state.tasks);
 	const dispatch = useDispatch()
+	const [search, setSearch] = useState([]);
+	const [searchTerm, setSearchTerm] = useState('');
 
   	useEffect(() => {
-		  dispatch(getTasks())
-  	}, [dispatch])
+				dispatch(getTasks());
+				const results = tasks.filter(character => {
+					return (
+						character.title.toLowerCase().includes(searchTerm.toLowerCase())
+					);
+				});
+				setSearch(results);
+				
+			}, [searchTerm,dispatch, tasks]);
 
+			const handleChange = e => {
+				setSearchTerm(e.target.value);
+			};
   	return (
     	<div>
-			<AdminDash />
-      	<Row>
+				<AdminDash />
+				<span >
+				<input style={{ marginLeft: '36%', width: '29%'}} placeholder='Search...' onChange={handleChange} type='text' name='searchTerm' value={searchTerm} />
+				</span>
+				{searchTerm.length === 0 ? (
+					<div>
+					<Row>
 				{tasks.length > 0
 					? tasks.map(task => (
 							<Col lg='4'>
@@ -25,8 +41,23 @@ const TaskList = () => {
 							</Col>
 					  ))
           		: null
-				}
-      	</Row>
+							}
+							</Row>
+					</div>
+				) : (<div>
+						<Row>
+				{search.length > 0
+					? search.map(task => (
+							<Col lg='4'>
+								<TaskCard title={task.title} description={task.description} assigned_to={task.volunteer_id} task={task} />
+							</Col>
+					  ))
+          		: null
+							}
+						</Row>
+						</div>
+						)}
+      	
 		</div>
 	);
 }
