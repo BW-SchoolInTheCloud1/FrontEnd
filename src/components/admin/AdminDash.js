087a8f85495775d95d773f7cac6d29f4873a839a
Crystal Martin <drcrystalm@gmail.com>
2020-03-04 22:01:43 -0600

    added styled cards to Register.js, changed colors, backgroundcolor and font color on Sign Up forms:sparkles:

--- src/components/admin/AdminDash.js
+++ src/components/admin/AdminDash.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import {  Container } from 'reactstrap';
-import DashNavBar from '../navs/DashNavBar';
-import { useParams } from 'react-router-dom';
-import { NavLink } from 'react-router-dom';
-
+import React from "react"
+import { Container } from "reactstrap"
+import DashNavBar from "../navs/DashNavBar"
+import { useParams } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 const AdminDash = () => {
-	const { id } = useParams();
-	
-	return (
-		<div>
-			<DashNavBar/>
-			<Container className='nestedNav'>
-				<NavLink className='navLink' to={`/admin-dash/${id}/showTask`} activeClassName='active'>
-					Show all tasks
-				</NavLink>
-				<NavLink className='navLink' to={`/admin-dash/${id}/adminVolunteer`} activeClassName='active'>
-					Show all Volunteers
-				</NavLink>
-			</Container>
-		</div>
-	);
-};
+    const { id } = useParams()
 
-export default AdminDash;
+    return (
+        <div>
+            <DashNavBar />
+            <Container className='nestedNav'>
+                <NavLink
+                    className='navLink2'
+                    to={`/admin-dash/${id}/showTask`}
+                    activeClassName='active'>
+                    Show all tasks
+                </NavLink>
+                <NavLink
+                    className='navLink2'
+                    to={`/admin-dash/${id}/adminVolunteer`}
+                    activeClassName='active'>
+                    Show all Volunteers
+                </NavLink>
+            </Container>
+        </div>
+    )
+}
+
+export default AdminDash
--- src/components/navs/Register.js
+++ src/components/navs/Register.js
@@ -1,30 +1,97 @@
-import React from 'react';
-import { Container } from 'reactstrap';
-import { NavLink } from 'react-router-dom';
-import NavBar from './NavBar';
+import React from "react"
+import {
+    Container,
+    Card,
+    CardTitle,
+    CardText,
+    CardImg,
+    CardImgOverlay,
+} from "reactstrap"
+import { NavLink } from "react-router-dom"
+import NavBar from "./NavBar"
+import Student from "../../images/Student.png"
+import Senior from "../../images/Senior.png"
+import Admin from "../../images/Admin.png"
 
 const Register = () => {
-	return (
-		<Container>
-			<NavBar />
-			<h1 className='nonForm' style={{ paddingTop: '40px' }}>
-				Choose a Role to Register
-			</h1>
-			<Container className='nestedNav'>
-				<NavLink to='/register/student' activeClassName='active' className='navLink'>
-					Students
-				</NavLink>
+    return (
+        <Container>
+            <NavBar />
+            <h1 className='nonForm' style={{ paddingTop: "40px" }}>
+                Choose a Role to Register
+            </h1>
+            <Container className='nestedNav'>
+                <Card className='register-card' inverse>
+                    <CardImg width='100%' src={Student} alt='Student icon' />
+                    <CardImgOverlay>
+                        <CardTitle>Student</CardTitle>
+                        <CardText
+                            style={{
+                                height: "65%",
+                            }}>
+                            > In college? In high school? In school at all? Need
+                            help? You are a student. Register Here!
+                        </CardText>
+                        <NavLink
+                            style={{
+                                marginLeft: "34%",
+                            }}
+                            to='/register/student'
+                            activeClassName='active'
+                            className='navLink'>
+                            Students
+                        </NavLink>
+                    </CardImgOverlay>
+                </Card>
 
-				<NavLink to='/register/volunteer' className='navLink' activeClassName='active'>
-					Volunteers
-				</NavLink>
+                <Card className='register-card' inverse>
+                    <CardImg width='100%' src={Senior} alt='Volunteer icon' />
+                    <CardImgOverlay>
+                        <CardTitle>Volunteer</CardTitle>
+                        <CardText
+                            style={{
+                                height: "65%",
+                            }}>
+                            > Want to help students from all over? Have extra
+                            time? Why not be a tutor volunteer? Sign up now!
+                        </CardText>
+                        <NavLink
+                            to='/register/volunteer'
+                            className='navLink'
+                            activeClassName='active'>
+                            Volunteers
+                        </NavLink>
+                    </CardImgOverlay>
+                </Card>
 
-				<NavLink to='/register/admin-signup' className='navLink' activeClassName='active'>
-					Admin
-				</NavLink>
-			</Container>
-		</Container>
-	);
-};
+                <Card className='register-card' inverse>
+                    <CardImg
+                        width='100%'
+                        src={Admin}
+                        alt='Administrator icon'
+                    />
+                    <CardImgOverlay>
+                        <CardTitle>Administrator</CardTitle>
+                        <CardText
+                            style={{
+                                height: "65%",
+                            }}>
+                            Want to help students connect with tutors to help
+                            close the achievement gap by connecting students
+                            with qualified availble volunteer tutors. This is
+                            your Role!
+                        </CardText>
+                        <NavLink
+                            to='/register/admin-signup'
+                            className='navLink'
+                            activeClassName='active'>
+                            Administrator
+                        </NavLink>
+                    </CardImgOverlay>
+                </Card>
+            </Container>
+        </Container>
+    )
+}
 
-export default Register;
+export default Register
